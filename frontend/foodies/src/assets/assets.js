3f0953f1aaea3c0dffd3100c58a8b17608c0625d
Osamo <osamovicky2021@gmail.com>
2025-10-22 12:11:16 +0300

    Frontend

---
+++ frontend/foodies/src/assets/assets.js
@@ -0,0 +1,45 @@
+import logo from "./logo.jpeg"
+import cart from './cart.jpg'
+import biryani from './biryani.jpeg'
+import burger from './burger.jpeg'
+import cakes from './cakes.jpeg'
+import icecream from './icecream.jpeg'
+import pizza from './pizza.jpeg'
+import rolls from './rolls.jpeg'
+import salad from './salad.jpeg'
+
+export const assets = {
+    logo,
+    cart
+}
+
+export const categories = [
+    {
+        category: 'Biryani',
+        icon: biryani
+    },
+    {
+        category: 'Burger',
+        icon: burger
+    },
+    {
+        category: 'Cakes',
+        icon: cakes
+    },
+    {
+        category: 'Ice Creams',
+        icon: icecream
+    },
+    {
+        category: 'Pizza',
+        icon: pizza
+    },
+    {
+        category: 'Rolls',
+        icon: rolls
+    },
+    {
+        category: 'Salad',
+        icon: salad
+    },
+]
