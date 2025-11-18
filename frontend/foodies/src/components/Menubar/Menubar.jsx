4d9f53dec119295848b4131dfc182ac4eed365dc
Osamo <osamovicky2021@gmail.com>
2025-11-18 19:15:57 +0300

    Frontend

--- frontend/foodies/src/components/Menubar/Menubar.jsx
+++ frontend/foodies/src/components/Menubar/Menubar.jsx
@@ -7,7 +7,7 @@ import { StoreContext } from '../../context/StoreContext'
 const Menubar = () => {
  
   const [active, setActive] = useState("home")
- const{quantities} = useContext(StoreContext)
+ const{quantities, token} = useContext(StoreContext)
  const uniqueItemsInCart = Object.values(quantities).filter(qty => qty > 0).length
  const navigate = useNavigate()
 
@@ -38,8 +38,22 @@ const Menubar = () => {
             <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning'>{uniqueItemsInCart}</span>
         </div>
         </Link>
-        <button className='btn btn-outline-primary'onClick={() => navigate("/login")}>Login</button>
+         {
+          !token ?
+          <>
+                  <button className='btn btn-outline-primary'onClick={() => navigate("/login")}>Login</button>
         <button className='btn btn-outline-success' onClick={() => navigate("/register")}>Register</button>
+          </> : 
+          <div className='dropdown text-end'>
+            <a href='' className='d-block link-body-emphasis text-decoration-none dropdown-toggle'  data-bs-toggle="dropdown" aria-expanded="false">
+               <img src="" alt="" width={32} height={32} className='rounded-circle' />
+            </a>
+            <ul className='dropdown-menu text-small'>
+              <li className='dropdown-item' onClick={() => navigate("/myorders")}>Orders</li>
+              <li className='dropdown-item' onClick={logout}>Logout</li>
+            </ul>
+          </div>
+         }
       </div>
     </div>
   </div>
